f3120dfbbf4dbf67eef125def20b574135968e0c
Isak <isak.gibran@gmail.com>
2019-09-17 09:16:40 +0000

    First push

---
+++ Repos/max_int.py
@@ -0,0 +1,8 @@
+num_int=int(input('Input a number: '))
+max_num=0
+while num_int>0 :
+    if num_int>max_num:
+        max_num=num_int
+    num_int=int(input('Input a number: ')) 
+print("The maximum is", max_num) 
+#!!!!!          
